3b7f44aa9ee21e1ab13533c8dbd113202e118f3a
Cedrick <cedrickokazaki@gmail.com>
2024-01-17 22:47:15 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,18 +121,6 @@
     });
   });
 
-  // JavaScript for opening and closing image popup
-  function openImagePopup(imageSrc) {
-    const popup = document.getElementById('popup');
-    const popupImage = document.getElementById('popup-image');
-    popupImage.src = imageSrc;
-    popup.style.display = 'flex';
-  }
-
-  function closeImagePopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none';
-  }
 
   
 })(jQuery);
